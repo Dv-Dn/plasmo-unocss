e824153db0159d18b968159d9bf830adcd5d9dc8
Denis Dv <dv-d3@hotmail.com>
2023-07-08 23:36:58 +0300

    init

---
+++ src/popup.svelte
@@ -0,0 +1,4 @@
+<script>
+    import '~styles'
+</script>
+<div class="bg-gray-100">Popup</div>
---
+++ uno.config.ts
@@ -0,0 +1,34 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  cli: {
+    entry: [
+      {
+        patterns: ['src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}'],
+        outFile: './src/styles/uno.css',
+      },
+    ],
+    // entry: {} // CliEntryItem | CliEntryItem[]
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono',
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
