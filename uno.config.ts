07b2b416d23aadefd14050a2b649a31a8a31a27c
Denis Dv <dv-d3@hotmail.com>
2023-07-13 00:49:17 +0300

    uno upd

--- uno.config.ts
+++ uno.config.ts
@@ -4,31 +4,36 @@ import {
   presetIcons,
   presetUno,
   presetWebFonts,
+  transformerAttributifyJsx,
   transformerDirectives,
-  transformerVariantGroup,
+  transformerVariantGroup
 } from 'unocss'
 
 export default defineConfig({
-  cli: {
-    entry: [
-      {
-        patterns: ['src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}'],
-        outFile: './src/styles/uno.css',
-      },
-    ],
-    // entry: {} // CliEntryItem | CliEntryItem[]
-  },
-  presets: [
-    presetUno(),
-    presetAttributify(),
-    presetIcons(),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
-    }),
-  ],
-  transformers: [transformerDirectives(), transformerVariantGroup()],
+	cli: {
+		entry: [
+			{
+				patterns: ['src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}'],
+				outFile: './src/styles/uno.css'
+			}
+		]
+		// entry: {} // CliEntryItem | CliEntryItem[]
+	},
+	presets: [
+		presetUno(),
+		presetAttributify(),
+		presetIcons(),
+		presetWebFonts({
+			fonts: {
+				sans: 'DM Sans',
+				serif: 'DM Serif Display',
+				mono: 'DM Mono'
+			}
+		})
+	],
+	transformers: [
+		transformerDirectives(),
+		transformerAttributifyJsx(),
+		transformerVariantGroup()
+	]
 })
